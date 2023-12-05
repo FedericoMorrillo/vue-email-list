758a6c8a7b94121c7990ecf3f5328e591f5bd064
FedericoMorrillo <federicomorrillo98@gmail.com>
2023-12-05 17:10:36 +0100

    feat: get api con axios

--- js/main.js
+++ js/main.js
@@ -1,1 +1,18 @@
 'use strict'
+
+const { createApp } = Vue;
+
+createApp({
+  data() {
+    return {
+      apiUrl: 'https://flynn.boolean.careers/exercises/api/random/mail',
+      email: null,
+    };
+  },
+  mounted() {
+    axios.get(this.apiUrl).then(response => {
+      this.email = response.data.response;
+      console.log(this.email);
+    });
+  },
+}).mount('#app');
